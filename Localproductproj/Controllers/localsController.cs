e418a53a1e5911a4545e3d5f6e438fdd9979a09b
surajsth <surakstha84@gmail.com>
2022-06-21 17:30:48 +0545

    hello

---
+++ Localproductproj/Controllers/localsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Localproductproj.Controllers
+{
+    public class localsController : Controller
+    {
+        public ViewResult Signup()
+        {
+            return View();
+        }
+        public ViewResult Login()
+        {
+            return View();
+        }
+        public ViewResult Products()
+        {
+            return View();
+        }
+        public ViewResult Aboutus()
+        {
+            return View();
+        }
+        public ViewResult Contactus()
+        {
+            return View();
+        }
+       
+    }
+}
+
---
+++ Localproductproj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Localproductproj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
